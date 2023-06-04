1ae952c84fd1ff9389bcf1ab5d54f005cf9d054c
taichan03 <tai.c.chan3@gmail.com>
2023-06-04 15:27:28 -0400

    Input

--- MedAssist/src/components/Summary.tsx
+++ MedAssist/src/components/Summary.tsx
@@ -5,6 +5,7 @@ import { useLazyGetMedicationInfoQuery } from "../services/medicationsApi";
 const Summary = () => {
   const [patientInfo, setPatientInfo] = useState({
     ID: "",
+    Diagnosis: "",
     description: "",
   });
   const [getMedicationInfo, { error, isFetching }] =
@@ -27,22 +28,15 @@ const Summary = () => {
     <section className="mt-16 w-full max-w-xl">
       {/* {search} */}
       <div className="flex flex-col w-full gap-2">
-        {/* Display ID and Description */}
-        {patientInfo.ID && (
-          <div>
-            <p>ID: {patientInfo.ID}</p>
-            <p>Description: {patientInfo.description}</p>
-          </div>
-        )}
         <form
           className="relative flex flex-col items-center"
           onSubmit={handleSubmit}
         >
-          <img
+          {/* <img
             src={linkIcon}
             alt="link-icon"
             className="absolute left-0 my-2 ml-3 w-5"
-          />
+          /> */}
           <input
             type="ID"
             placeholder="Patient ID:"
@@ -53,6 +47,24 @@ const Summary = () => {
             required
             className="url_input peer" // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
           />
+          <input
+            type="Text"
+            placeholder="Patient Diagnosis:"
+            value={patientInfo.Diagnosis}
+            onChange={(e) =>
+              setPatientInfo({ ...patientInfo, Diagnosis: e.target.value })
+            }
+            required
+            className="url_input peer" // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+          />
+          {/* Display ID and Description */}
+          {patientInfo.ID && (
+            <div>
+              <p>ID: {patientInfo.ID}</p>
+              <p>Diagnosis: {patientInfo.Diagnosis}</p>
+              <p>Description: {patientInfo.description}</p>
+            </div>
+          )}
           <div className="flex justify-center mt-2">
             <button
               type="submit"

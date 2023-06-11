0e30dfc734e1ade43c9101ae5b8a43b5c2ad6f41
Ryan R Rogers <ryan.rogers.developer@gmail.com>
2023-06-11 10:35:55 -0400

    Break patient summary into own component

--- MedAssist/src/components/form_components/NewPatientForm.tsx
+++ MedAssist/src/components/form_components/NewPatientForm.tsx
@@ -31,7 +31,9 @@ const NewPatientForm = ({
 }: NewPatientFormData) => {
   useEffect(() => {
     const patientInfoFromLocalStorage = JSON.parse(
-      localStorage.getItem("patientInfos") ?? ""
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+      localStorage.getItem("patientInfos")
     );
     if (patientInfoFromLocalStorage) {
       setAllPatientInfo(patientInfoFromLocalStorage);
@@ -77,70 +79,10 @@ const NewPatientForm = ({
   };
 
   return (
-    <section className="mt-16 w-full max-w-xl">
+    <section>
       {/* {search} */}
-      <div className="flex flex-col w-full gap-2">
-        {/* Display ID and Description */}
-        <div className="my-1 max-w-full flex justify-center items-center">
-          {getMedicationInfo.isFetching ? (
-            <img
-              src={loader}
-              alt="loader"
-              className="w-20 h-20 object-contain"
-            />
-          ) : getMedicationInfo.error ? (
-            <p className="font-inter font-bold text-black text-center">
-              Well, that wasn't supposed to happen...
-              <br />
-              <span className="font-satoshi font-normal text-gray-700">
-                {getMedicationInfo.error?.data?.error}
-              </span>
-            </p>
-          ) : (
-            patientInfo.Description && (
-              <div className="flex flex-col gap-3">
-                <h2 className="font-satoshi font-bold text-gray-600 text-xl">
-                  Patient <span className="blue_gradient">Summary</span>
-                </h2>
-                <div className="summary_box">
-                  <p className="font-inter font-medium text-sm text-gray-700">
-                    <label
-                      htmlFor="ageInput"
-                      className="block font-latoBold text-sm pb-2"
-                    >
-                      {" "}
-                      <b>Patient ID: </b> {patientInfo.ID}
-                    </label>
-                    <label
-                      htmlFor="ageInput"
-                      className="block font-latoBold text-sm pb-2"
-                    >
-                      <b>Diagnosis: </b> {patientInfo.Diagnosis}{" "}
-                      {patientInfo.OtherDiagnosis}
-                    </label>
-                    <label
-                      htmlFor="ageInput"
-                      className="block font-latoBold text-sm pb-2"
-                    >
-                      {" "}
-                      <b>Age: </b>
-                      {patientInfo.Age}
-                    </label>
-                    <label
-                      htmlFor="ageInput"
-                      className="block font-latoBold text-sm pb-2"
-                    >
-                      <b>{patientInfo.Description}</b>
-                    </label>
-                  </p>
-                </div>
-              </div>
-            )
-          )}
-        </div>
-
+      <div>
         <br />
-
         <div>
           {" "}
           <h2 className="font-satoshi font-bold text-gray-600 text-xl">

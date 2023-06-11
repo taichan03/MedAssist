2a5279efdcf88b08b18ce3985f9a9bd0902675ce
taichan03 <tai.c.chan3@gmail.com>
2023-06-11 00:22:59 -0400

    correct new title

--- MedAssist/src/components/Summary.tsx
+++ MedAssist/src/components/Summary.tsx
@@ -87,7 +87,7 @@ const Summary = () => {
             patientInfo.Description && (
               <div className="flex flex-col gap-3">
                 <h2 className="font-satoshi font-bold text-gray-600 text-xl">
-                  Article <span className="blue_gradient">Summary</span>
+                  Patient <span className="blue_gradient">Summary</span>
                 </h2>
                 <div className="summary_box">
                   <p className="font-inter font-medium text-sm text-gray-700">

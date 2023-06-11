93bcf90dff4152487b0dee86a125bcb141325a89
Ryan R Rogers <ryan.rogers.developer@gmail.com>
2023-06-11 00:12:53 -0400

    Change project name to Balancer (was MedAssist).

--- MedAssist/src/components/Header.tsx
+++ MedAssist/src/components/Header.tsx
@@ -16,10 +16,10 @@ const Header = () => {
 
       <h1 className="head_text">
         {/* AI-powered Bipolar Medication: <br className="max-md:hidden" /> */}
-        <span className="orange_gradient">MedAssist</span>
+        <span className="orange_gradient">Balancer</span>
       </h1>
       <h2 className="desc">
-        MedAssist is an innovative AI-powered web application designed to assist
+        Balancer is an innovative AI-powered web application designed to assist
         psychiatrists in selecting the most appropriate bipolar medication for
         first-time patients. It is open-source and free to use.
       </h2>

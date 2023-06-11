fbce8162a5be7e50e2d96df51ada9bd1144cec3f
taichan03 <tai.c.chan3@gmail.com>
2023-06-10 23:59:06 -0400

    added Summary and history

--- MedAssist/src/components/Summary.tsx
+++ MedAssist/src/components/Summary.tsx
@@ -1,6 +1,7 @@
 import { useState, FormEvent, ChangeEvent, useEffect } from "react";
 import { useLazyGetMedicationInfoQuery } from "../services/medicationsApi";
 import PatientIDInput from "./form_components/PatientID.tsx";
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Summary = () => {
   const [patientInfo, setPatientInfo] = useState({
@@ -10,20 +11,19 @@ const Summary = () => {
     Description: "",
     Age: 18,
   });
-  const[allPatientInfo, setAllPatientInfo] = useState([]);
+  const [allPatientInfo, setAllPatientInfo] = useState([]);
 
   const [getMedicationInfo, { error, isFetching }] =
     useLazyGetMedicationInfoQuery();
 
   useEffect(() => {
-    const patienInfoFromLocalStorage = JSON.parse(
-      localStorage.getItem('patientInfo')
-    )
-    if(patienInfoFromLocalStorage){
-      setAllPatientInfo(patienInfoFromLocalStorage)
+    const patientInfoFromLocalStorage = JSON.parse(
+      localStorage.getItem("patientInfos")
+    );
+    if (patientInfoFromLocalStorage) {
+      setAllPatientInfo(patientInfoFromLocalStorage);
     }
-
-  }, [])
+  }, []);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -32,21 +32,24 @@ const Summary = () => {
     if (data?.description) {
       const newDescription = { ...patientInfo, Description: data.description };
 
-      const updatedAllPatientInfo = [newDescription, ...allPatientInfo]
+      const updatedAllPatientInfo = [newDescription, ...allPatientInfo];
       setPatientInfo(newDescription);
 
       console.log("Logged new description");
-      setAllPatientInfo(updatedAllPatientInfo)
+      setAllPatientInfo(updatedAllPatientInfo);
 
-    }
-    else{
+      localStorage.setItem(
+        "patientInfos",
+        JSON.stringify(updatedAllPatientInfo)
+      );
+    } else {
       console.log("No description came back");
     }
   };
 
   const handleDiagnosisChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const selectedValue = e.target.value;
-    if (selectedValue === 'Other') {
+    if (selectedValue === "Other") {
       setPatientInfo({
         ...patientInfo,
         Diagnosis: selectedValue,
@@ -55,7 +58,7 @@ const Summary = () => {
       setPatientInfo({
         ...patientInfo,
         Diagnosis: selectedValue,
-        OtherDiagnosis: '', // Reset the OtherDiagnosis value
+        OtherDiagnosis: "", // Reset the OtherDiagnosis value
       });
     }
   };
@@ -64,25 +67,28 @@ const Summary = () => {
     <section className="mt-16 w-full max-w-xl">
       {/* {search} */}
       <div className="flex flex-col w-full gap-2">
-        <form
-          className=""
-          onSubmit={handleSubmit}
-        >
+        <form className="" onSubmit={handleSubmit}>
           {/* <img
             src={linkIcon}
             alt="link-icon"
             className="absolute left-0 my-2 ml-3 w-5"
           /> */}
-          <PatientIDInput patientInfo={patientInfo} setPatientInfo={setPatientInfo}></PatientIDInput>
+          <PatientIDInput
+            patientInfo={patientInfo}
+            setPatientInfo={setPatientInfo}
+          ></PatientIDInput>
           <div className=" mt-5">
-          <label htmlFor="ageInput" className="block font-latoBold text-sm pb-2">
-          Diagnosis:
-          </label>
+            <label
+              htmlFor="ageInput"
+              className="block font-latoBold text-sm pb-2"
+            >
+              Diagnosis:
+            </label>
             <select
-                value={patientInfo.Diagnosis}
-                onChange={handleDiagnosisChange}
-                required
-                className="url_input peer"
+              value={patientInfo.Diagnosis}
+              onChange={handleDiagnosisChange}
+              required
+              className="url_input peer"
             >
               <option value="">Select a diagnosis</option>
               <option value="Bipolar I">Bipolar I</option>
@@ -90,36 +96,41 @@ const Summary = () => {
               <option value="Cyclothymic">Cyclothymic</option>
               <option value="Other">Other</option>
             </select>
-            {patientInfo.Diagnosis === 'Other' && (
-                <input
-                    type="text"
-                    placeholder="Please specify"
-                    value={patientInfo.OtherDiagnosis}
-                    onChange={(e) =>
-                        setPatientInfo({ ...patientInfo, OtherDiagnosis: e.target.value })
-                    }
-                    required
-                    className="url_input peer"
-                />
+            {patientInfo.Diagnosis === "Other" && (
+              <input
+                type="text"
+                placeholder="Please specify"
+                value={patientInfo.OtherDiagnosis}
+                onChange={(e) =>
+                  setPatientInfo({
+                    ...patientInfo,
+                    OtherDiagnosis: e.target.value,
+                  })
+                }
+                required
+                className="url_input peer"
+              />
             )}
           </div>
           <div className="items-center mt-5">
-          <label htmlFor="ageInput" className="block font-latoBold text-sm pb-2">
-            Age:
-          </label>
-          <input
-            id="ageInput"
-            type="number"
-            value={patientInfo.Age}
-            onChange={(e) =>
-              setPatientInfo({ ...patientInfo, Age: Number(e.target.value) })
-            }
-            required
-            className="url_input peer"
-          />
+            <label
+              htmlFor="ageInput"
+              className="block font-latoBold text-sm pb-2"
+            >
+              Age:
+            </label>
+            <input
+              id="ageInput"
+              type="number"
+              value={patientInfo.Age}
+              onChange={(e) =>
+                setPatientInfo({ ...patientInfo, Age: Number(e.target.value) })
+              }
+              required
+              className="url_input peer"
+            />
           </div>
-          
-         
+
           <div className="flex justify-center mt-5">
             <button
               type="submit"
@@ -130,16 +141,90 @@ const Summary = () => {
           </div>
         </form>
         <br />
-
-         {/* Display ID and Description */}
-         {patientInfo.ID && (
-            <div>
-              <p>ID: {patientInfo.ID}</p>
-              <p>Diagnosis: {patientInfo.Diagnosis}</p>
-              <p>Description: {patientInfo.Description}</p>
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allPatientInfo.reverse().map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setPatientInfo(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.ID}
+              </p>
             </div>
-          )}
+          ))}
+        </div>
       </div>
+      <br />
+      {/* Display ID and Description */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            Well, that wasn't supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          patientInfo.Description && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  <label
+                    htmlFor="ageInput"
+                    className="block font-latoBold text-sm pb-2"
+                  >
+                    {" "}
+                    <b>Patient ID: </b> {patientInfo.Diagnosis} {patientInfo.ID}
+                  </label>
+                  <label
+                    htmlFor="ageInput"
+                    className="block font-latoBold text-sm pb-2"
+                  >
+                    <b>Diagnosis: </b> {patientInfo.Diagnosis}{" "}
+                    {patientInfo.OtherDiagnosis}
+                  </label>
+                  <label
+                    htmlFor="ageInput"
+                    className="block font-latoBold text-sm pb-2"
+                  >
+                    {" "}
+                    <b>Age: </b>
+                    {patientInfo.Age}
+                  </label>
+                  <label
+                    htmlFor="ageInput"
+                    className="block font-latoBold text-sm pb-2"
+                  >
+                    <b>{patientInfo.Description}</b>
+                  </label>
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+
+      {/* {patientInfo.ID && (
+        <div>
+          <p>ID: {patientInfo.ID}</p>
+          <p>Diagnosis: {patientInfo.Diagnosis}</p>
+          <p>Description: {patientInfo.Description}</p>
+        </div>
+      )} */}
     </section>
   );
 };
--- MedAssist/src/components/form_components/PatientID.tsx
+++ MedAssist/src/components/form_components/PatientID.tsx
@@ -1,37 +1,39 @@
-import React, { useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 interface PatientInfo {
-    ID: string;
-    Diagnosis: string;
-    OtherDiagnosis: string;
-    Description: string;
-    Age: number;
+  ID: string;
+  Diagnosis: string;
+  OtherDiagnosis: string;
+  Description: string;
+  Age: number;
 }
 
 interface PatientIDInputProps {
-    patientInfo: PatientInfo;
-    setPatientInfo: React.Dispatch<React.SetStateAction<PatientInfo>>;
+  patientInfo: PatientInfo;
+  setPatientInfo: React.Dispatch<React.SetStateAction<PatientInfo>>;
 }
 
 function PatientIDInput({ patientInfo, setPatientInfo }: PatientIDInputProps) {
-    useEffect(() => {
-        const generatedGuid = uuidv4();
-        setPatientInfo({ ...patientInfo, ID: generatedGuid });
-    }, []);
+  useEffect(() => {
+    const generatedGuid = uuidv4();
+    setPatientInfo({ ...patientInfo, ID: generatedGuid });
+  }, []);
 
-    return (
-        <div className="">
-            <label htmlFor="name"  className="block font-latoBold text-sm pb-2">Patient ID: </label>
-            <input
-                type="text"
-                placeholder="Patient ID:"
-                value={patientInfo.ID}
-                readOnly
-                className="url_input peer w-1/2"
-            />
-        </div>
-    );
+  return (
+    <div className="">
+      <label htmlFor="name" className="block font-latoBold text-sm pb-2">
+        Patient ID:{" "}
+      </label>
+      <input
+        type="text"
+        placeholder="Patient ID:"
+        value={patientInfo.ID}
+        readOnly
+        className="url_input peer w-1/2"
+      />
+    </div>
+  );
 }
 
 export default PatientIDInput;
